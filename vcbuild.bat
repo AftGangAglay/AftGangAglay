8b92545183a7f89173181b75d03cd999d0b40715
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2024-01-12 22:57:55 +0000

    misc: remove leftover references to TIFF

--- vcbuild.bat
+++ vcbuild.bat
@@ -7,7 +7,7 @@ set CFLAGS=/nologo /TC /Zi /Od
 
 set CFLAGS=%CFLAGS% /I include /I vendor\afeirsa\include
 set CFLAGS=%CFLAGS% /I vendor\www\Library\Implementation /I vendor\support
-set CFLAGS=%CFLAGS% /I vendor\python\src /I vendor\libtiff\libtiff
+set CFLAGS=%CFLAGS% /I vendor\python\src
 
 set CFLAGS=%CFLAGS% /D_DEBUG /DAF_WGL /DGL10_COMPAT /DNO_EXT /D_WINDOWS
 set CFLAGS=%CFLAGS% /DNO_UNIX_IO /DNO_GROUPS /DUSE_STDLIB
@@ -19,9 +19,7 @@ set LDFLAGS=%LDFLAGS% Ws2_32.lib
 rem windres -i res\aga.rc -o res\aga.res
 rem windres -i res\aga.rc -o res\aga.o
 
-set TIFF=vendor\libtiff\libtiff
 set PGENDIR=vendor\support
-cl.exe %CFLAGS% %TIFF%\mkg3states.c /Fe:%TIFF%\mkg3states.exe
 cl.exe %CFLAGS% %PGENDIR%\pgen_unity.c /Fe:%PGENDIR%\pgenmain.exe
 
 set OBJTMP="%TMP%\bat~%RANDOM%.tmp"

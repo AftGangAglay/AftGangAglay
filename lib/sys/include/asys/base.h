f72321bc57a4a33c00fec45c85bd3a8e259e60f3
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2025-04-14 12:48:24 +0100

    aftgangaglay: standardize fixed buffer size across platforms

--- lib/sys/include/asys/base.h
+++ lib/sys/include/asys/base.h
@@ -99,12 +99,8 @@ typedef asys_native_long_t asys_time_t;
 
 typedef enum asys_bool { ASYS_TRUE = 1, ASYS_FALSE = 0 } asys_bool_t;
 
-#ifdef ASYS_WIN32
 /* NOTE: `wsprintf''s maximum buffer size is `1024'. */
 # define ASYS_FIXED_BUFFER_SIZE (1024 + 1)
-#else
-# define ASYS_FIXED_BUFFER_SIZE (2048 + 1)
-#endif
 
 /*
  * NOTE: This exists for cases where we are forced to use fixed size buffers
